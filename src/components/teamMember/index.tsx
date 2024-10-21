66fa5458265c559033edbf50a94417182688aa83
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2024-10-21 09:22:39 +0700

    team members mobile view enhance

--- src/components/teamMember/index.tsx
+++ src/components/teamMember/index.tsx
@@ -10,12 +10,12 @@ interface Props {
 
 export default function TeamMember(props : Props) {
     return (
-        <div className="flex md:h-[200px] my-4">
-            <div className="w-[150] flex items-center ">
+        <div className="flex flex-col md:flex-row md:h-[200px] my-4">
+            <div className="w-[150] flex items-center justify-center mb-4 md:mb-0">
                 <Image className="rounded-[20px]" src={props.imageUrl} alt={props.name} width={150} height={200}></Image>
             </div>
             <div className="flex-1 flex flex-col h-[100%] ml-4 justify-center text-justify">
-                <div className="flex flex-col">
+                <div className="flex flex-col items-center md:items-start mb-4 md:mb-0">
                     <span className="font-semibold">{props.name}</span>
                     <span className="italic">{props.role}</span>
                     <span className="underline">{props.contact}</span>
