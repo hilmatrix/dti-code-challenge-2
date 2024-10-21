a6589a9ffc4ec30b97085272e912bc6b689c5a3e
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2024-10-21 09:37:13 +0700

    add back to top button in right menu

--- src/components/rightMenu/index.tsx
+++ src/components/rightMenu/index.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Link {
   href : string;
@@ -16,6 +16,27 @@ interface Props {
 
 export default function RightMenu(props : Props) {
     const [showRightMenu, setShowRightMenu] = useState(false);
+    const [isBottom, setIsBottom] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+          const scrollTop = window.scrollY;
+          const windowHeight = window.innerHeight;
+          const fullHeight = document.documentElement.scrollHeight;
+  
+          if (scrollTop + windowHeight >= fullHeight) {
+            setIsBottom(true); 
+          } else {
+            setIsBottom(false);
+          }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+      
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
     return (
         <div className="fixed right-0 top-0 z-5 w-full h-full pointer-events-none">
@@ -33,11 +54,28 @@ export default function RightMenu(props : Props) {
 
             <div className="absolute bottom-0 right-0">
               <div className='m-10 mb-20 w-[74px] h-[74px] bg-light-gray rounded-full flex justify-center items-center'>
-              <button data-testid="show-right-menu" onClick={() => {setShowRightMenu(!showRightMenu)}} className="flex justify-center items-center w-[70px] h-[70px] bg-[#000000] rounded-full pointer-events-auto">
-                <FontAwesomeIcon icon={faBars} color="white" size="2x" />
-              </button>
+                <button data-testid="show-right-menu" onClick={() => {setShowRightMenu(!showRightMenu)}} className="flex justify-center items-center w-[70px] h-[70px] bg-[#000000] rounded-full pointer-events-auto">
+                  <FontAwesomeIcon icon={faBars} color="white" size="2x" />
+                </button>
               </div>
             </div>
+
+           {isBottom ? 
+              (
+            <div className="absolute bottom-0 right-20">
+              <div className='m-10 mb-20 w-[74px] h-[74px] bg-light-gray rounded-full flex justify-center items-center'>
+                <Link href="#">
+                   <div className="flex justify-center items-center w-[70px] h-[70px] bg-[#000000] rounded-full pointer-events-auto">
+                     <FontAwesomeIcon icon={faArrowUp} color="white" size="2x" />
+                   </div>
+                </Link>
+              </div>
+            </div>
+              )
+              :
+              (<></>)
+           }
+            
         </div>
     );
 }
