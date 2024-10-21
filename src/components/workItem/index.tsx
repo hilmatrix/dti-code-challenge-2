c1a218f624e4813f61681d674374e459bab1d756
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2024-10-21 10:28:50 +0700

    add play button

--- src/components/workItem/index.tsx
+++ src/components/workItem/index.tsx
@@ -9,13 +9,17 @@ interface Parameters {
 
 export default function Work(parameters : Parameters) {
     return (
-        <div className="h-[150px] md:h-[100px] my-4 flex flex-col md:flex-row">
+        <div className="h-[180px] md:h-[100px] my-4 flex flex-col md:flex-row">
             <div className='flex justify-center md:justify-left'>
                 <Image src={parameters.imageLink} alt={parameters.workName} width={300} height={100} className='rounded-[12px]'></Image>
             </div>
             <div className='flex flex-col items-center md:items-start md:m-4'>
-                <span className='font-semibold'>{parameters.workName}</span>
-                <Link href={parameters.hyperLink} className='underline'>Link</Link>
+                <span className='font-semibold '>{parameters.workName}</span>
+                <Link href={parameters.hyperLink} className='text-white' target='_blank'>
+                <div className='bg-[#000000] w-24 h-10 flex justify-center items-center rounded-full my-2'>
+                    Play Now
+                </div>
+                </Link>
             </div>
         </div>
     );
